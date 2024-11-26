2c7e6615f585f0d9cf2f318a643dfbece88f366f
Sérgio Borges <sbintcel123@gmail.com>
2024-11-25 22:01:57 -0300

    Lista de Serviços

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,6 +10,37 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final List<Map<String, dynamic>> servicos = [
+    {
+      "titulo": "Corte de Cabelo",
+      "descricao":
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley",
+      "preco": 50.00,
+      "imagemUrl": "https://via.placeholder.com/150"
+    },
+    {
+      "titulo": "Manicure e Pedicure",
+      "descricao":
+          "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution",
+      "preco": 35.00,
+      "imagemUrl": "https://via.placeholder.com/150"
+    },
+    {
+      "titulo": "Mecânico de Auto",
+      "descricao":
+          "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin",
+      "preco": 100.00,
+      "imagemUrl": "https://via.placeholder.com/150"
+    },
+    {
+      "titulo": "Aula de Informática",
+      "descricao":
+          "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly",
+      "preco": 150.00,
+      "imagemUrl": "https://via.placeholder.com/150"
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,43 +51,95 @@ class _HomePageState extends State<HomePage> {
       ),
       backgroundColor: Colors.white,
       drawer: Drawer(
-          child: ListView(
-        children: const [
-          SizedBox(
-            height: 100,
-            child: DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.orange,
+        child: ListView(
+          children: const [
+            SizedBox(
+              height: 100,
+              child: DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.orange,
+                ),
+                padding: EdgeInsetsDirectional.symmetric(
+                    vertical: 28, horizontal: 16),
+                child: Text("Olá, Sérgio",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    )),
               ),
-              padding:
-                  EdgeInsetsDirectional.symmetric(vertical: 28, horizontal: 16),
-              child: Text("Olá, Sérgio",
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  )),
             ),
-          ),
-          ListTile(
-            leading: Icon(Icons.login),
-            // trailing: Icon(Icons.login),
-            title: Text("Login"),
-          ),
-          ListTile(
-            leading: Icon(Icons.list),
-            title: Text("Serviços"),
-          ),
-          ListTile(
-            leading: Icon(Icons.list),
-            title: Text("Dúvidas"),
-          ),
-          Divider(),
-          ListTile(
-            leading: Icon(Icons.list),
-            title: Text("Sobre o BookMeNow"),
-          ),
-        ],
-      )),
+            ListTile(
+              leading: Icon(Icons.login),
+              // trailing: Icon(Icons.login),
+              title: Text("Login"),
+            ),
+            ListTile(
+              leading: Icon(Icons.list),
+              title: Text("Serviços"),
+            ),
+            ListTile(
+              leading: Icon(Icons.list),
+              title: Text("Dúvidas"),
+            ),
+            Divider(),
+            ListTile(
+              leading: Icon(Icons.list),
+              title: Text("Sobre o BookMeNow"),
+            ),
+          ],
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: servicos.length,
+        itemBuilder: (context, index) {
+          final servico = servicos[index];
+          return Card(
+            elevation: 0.5,
+            margin: const EdgeInsets.all(8.0),
+            color: const Color(0xFFfcfcfc),
+            child: Row(
+              children: [
+                Image.network(
+                  servico['imagemUrl'],
+                  width: 100,
+                  height: 100,
+                  fit: BoxFit.cover,
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          servico['titulo'],
+                          style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.left,
+                        ),
+                        Text(
+                          servico['descricao'],
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        Text(
+                          'R\$ ${servico['preco'].toStringAsFixed(2)}',
+                          style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
